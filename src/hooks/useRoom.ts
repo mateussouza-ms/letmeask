dc2b0ee8b6dd401d5de48c5ad67ddcc114df9b8d
Mateus Souza <mateus_souza_teles@hotmail.com>
2021-06-24 22:31:08 -0300

    Create useRoom hook
    Create hook to abstract shared functionality from room pages.

---
+++ src/hooks/useRoom.ts
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+
+import { database } from "../services/firebase";
+
+type FirebaseQuestions = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: string;
+    };
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+  }
+>;
+
+type QuestionType = {
+  id: string;
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  isHighlighted: boolean;
+  isAnswered: boolean;
+};
+
+export function useRoom(roomId: string) {
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.on("value", (room) => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighlighted: value.isHighlighted,
+            isAnswered: value.isAnswered,
+          };
+        }
+      );
+
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    });
+  }, [roomId]);
+
+  return { title, questions };
+}
